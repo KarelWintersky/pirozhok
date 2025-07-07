c6ed1fa1dfe52856f663cbeb3c54875d8b63cd0e
Karel Wintersky <karel@wintersky.ru>
2025-07-08 00:50:33 +0300

    try save sqlite

--- sethook.php
+++ sethook.php
@@ -3,12 +3,10 @@
 $config = require __DIR__ . '/config.php';
 
 $bot_api_key = $config['TOKEN'];
-$domain = 'pirozhok.dev.wombatrpg.ru';
-$domain = 'pirozhok.bot.wombatrpg.ru';
-$target = 'bot_nut.php';
-// $target = 'bot_legacy.php';
+$domain = $config['WEBHOOK_DOMAIN'];
+$script = $config['WEBHOOK_SCRIPT'];
 
-$url = "https://api.telegram.org/bot{$bot_api_key}/setWebhook?url={$domain}/{$target}";
+$url = "https://api.telegram.org/bot{$bot_api_key}/setWebhook?url={$domain}/{$script}";
 
 var_dump(json_decode(file_get_contents($url), true));
 
